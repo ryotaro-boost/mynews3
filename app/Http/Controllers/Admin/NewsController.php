eb8b186ddc905a376f05f0e48e522aa6939f2047
ryotaro <ryotaro.t.0727@gmail.com>
2020-03-04 07:40:12 +0000

    2020/3/4

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\News;
+
+class NewsController extends Controller
+{
+    public function add()
+    {
+      return view('admin.news.create');
+  }
+
+  public function create(Request $request)
+  {
+      $this->validate($request, News::$rules);
+      $news = new News;
+      $form = $request->all();
+      
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
+  }  
+}
